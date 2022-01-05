94fc3e8d9b7984b5ebd4e742da97cf9dd02adc31
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-01-05 20:23:50 +0200

    fix-feedback-input-with-mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,41 @@
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const LOCALSTORAGE_KEY = "feedback-form-state";
-const formData = {}
+const email = formEl.elements.email;
+const message = formEl.elements.message;
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 repairTextInput();
 
 formEl.addEventListener('input', throttle(onFormInput, 500));
-function onFormInput(e) { 
-    e.preventDefault();
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-    formData[e.target.name] = e.target.value;
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
-formEl.addEventListener("submit", onFormSubmit);
-function onFormSubmit(e) { 
-    e.preventDefault();
-    const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+function onFormSubmit(e) {
+  e.preventDefault();
 
-    if (parseMessage?.email && parseMessage?.message) {
-        console.log(parseMessage);
-        localStorage.removeItem(LOCALSTORAGE_KEY);
-        e.currentTarget.reset();  
-    } else { alert('заполните все поля'); };
+  if (email.value && message.value) {
+    console.log({ email: email.value, message: message.value });
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    e.currentTarget.reset();
+  } else {
+    alert('заполните все поля');
+  }
 }
 
-function repairTextInput() { 
-    const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+function repairTextInput() {
+  const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-    if (parseMessage?.message) {
-        formEl.message.value = parseMessage.message;
-        formData.message = formEl.message.value
-    };
+  if (!parseMessage) return;
 
-    if (parseMessage?.email) {
-        formEl.email.value = parseMessage.email;
-        formData.email = formEl.email.value
-    };
+  message.value = parseMessage.message;
+  email.value = parseMessage.email;
 }
