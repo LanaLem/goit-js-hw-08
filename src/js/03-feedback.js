73571e74806a7eed4215794d786a3289ce22caad
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-01-05 11:46:04 +0200

    feedback-add-variables-name

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -16,24 +16,24 @@ function onFormInput(e) {
 formEl.addEventListener("submit", onFormSubmit);
 function onFormSubmit(e) { 
     e.preventDefault();
+    const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-    if (JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))?.email && JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))?.message) {
-        console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    if (parseMessage?.email && parseMessage?.message) {
+        console.log(parseMessage);
         localStorage.removeItem(LOCALSTORAGE_KEY);
         e.currentTarget.reset();  
     } else { alert('заполните все поля'); };
 }
 
 function repairTextInput() { 
-    const saveMessage = localStorage.getItem(LOCALSTORAGE_KEY);
-    const parseMessage = JSON.parse(saveMessage);
+    const parseMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-    if (JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))?.message) {
+    if (parseMessage?.message) {
         formEl.message.value = parseMessage.message;
         formData.message = formEl.message.value
     };
 
-    if (JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))?.email) {
+    if (parseMessage?.email) {
         formEl.email.value = parseMessage.email;
         formData.email = formEl.email.value
     };
